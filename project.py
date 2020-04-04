c639056cb3115e9692eb92c3a7f67a7b902f8994
Rohit Yadav <36873594+rky71992@users.noreply.github.com>
2020-04-04 13:49:41 +0530

    2 clustering techniques added

--- project.py
+++ project.py
@@ -1,64 +1,78 @@
-#
-#importing package dependencies
-import pandas as pd
+#importing packages dependencies
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-
-
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
+from sklearn.preprocessing import LabelEncoder, normalize, StandardScaler
 
 #Preparing Data
-#from dataset_file, extract #1Reg No, Course, Grades and Branch = Food Science and Technology
-initial_dataset = pd.read_csv("dataset_file.csv",usecols=[0,1,2,3,4,5,6,8,9])
-#initial_dataset contains all rows, extracting rows with 'MHRDName' == 'Master of Science (Food Science and Technology)'
-print("Total number of Rows loaded:",initial_dataset.shape)
-corrected_dataset = initial_dataset[initial_dataset['MHRDName'] == 'Master of Science (Food Science and Technology)']
-print("Rows of MHRDName =Master of Science Food Science and Technology:",corrected_dataset.shape)
-corrected_dataset = corrected_dataset.drop('MHRDName',axis=1)
-corrected_dataset = corrected_dataset.dropna(axis=0)
-print("Rows after droping NaNa rows:",corrected_dataset.shape)
-print("Colums headings:\n",corrected_dataset.columns.values)
+#Only columns are loaded which are required for process
+df = pd.read_csv("dataset_file.csv",usecols=[0,1,2,3,4,5,6,8,9])
+print("Shape of data loaded:",df.shape)
+print("Datahead:\n",df.head())
 
-labelEncoder = LabelEncoder()
-labelEncoder.fit(corrected_dataset['Grade'])
-corrected_dataset['Grade'] = labelEncoder.transform(corrected_dataset['Grade'])
+#Only rows are selected where MHRDName == Master of Science (Food Science and Technology)
+df = df[df['MHRDName'] == 'Master of Science (Food Science and Technology)']
+print("Rows of MHRDName = Master of Science (Food Science and Technology):",df.shape[0])
+df = df.drop('MHRDName',axis=1)
 
-#dividing into test train
-train, test = train_test_split(corrected_dataset, test_size=0.2)
-#print("Test shape:",test.shape,"Training Shape:",train.shape)
-#x = df.iloc[:, [0,1,2,3]].values
-#SInce its a clustring algorithm we are droping grade column from training dataset
-train = train.drop(['Grade','Termid','Regd No','Course'],axis=1)
-#kmeans = KMeans(n_clusters=4)
-#kmeans.fit(corrected_dataset)
-print(corrected_dataset.head())
-x = corrected_dataset.iloc[:,[4,5,6,7]].values
-kmeans = KMeans(n_clusters=2, max_iter=600, algorithm = 'auto')
-kmeans.fit(x)
-y_kmeans5 = kmeans.fit_predict(x)
-print(y_kmeans5)
-print(kmeans.cluster_centers_)
-Error =[]
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters = i).fit(x)
-    kmeans.fit(x)
-    Error.append(kmeans.inertia_)
-#import matplotlib.pyplot as plt
-plt.plot(range(1, 11), Error)
-plt.title('Elbow method')
-plt.xlabel('No of clusters')
-plt.ylabel('Error')
+#Checking for missing values rows, and droping missing value rows in this case
+print("Data with NaNa value:\n",df.isnull().sum())
+df = df.dropna(axis=0)
+print("Data after removing NaNa value:\n",df.isnull().sum())
+print("Rows after droping NaNa rows:",df.shape[0])
+
+#Adding Labelencoders, generating uid for identification, droping processed columns and adding unique student_id
+df['student_id'] = df["Termid"].astype(str) + "" + df["Regd No"].astype(str) + "" + df["Course"].astype(str)
+#Trimimg unwanted columns
+df = df.drop(['Termid','Regd No','Course'], axis=1)
+
+#adding label encoder to grades for processing
+unique_grades = len(df['Grade'].unique())
+le = LabelEncoder()
+df['Grade'] = le.fit_transform(df['Grade'].values)
+print('After pre-processing Step 4\n',df.head())
+
+#Clustring Techniques
+#Kmeans: Unsupervised learning
+
+kmeans_df = df.copy()
+kmeans_df = df.drop(['student_id','Grade'],axis=1)
+print("Columns in KMeans data input\n",kmeans_df.columns.values)
+c = ['red','blue','green','yellow','orange','black','indigo']
+distortion=[]
+
+for i in range(2,unique_grades):
+    kmeans = KMeans(n_clusters=i)
+    y_km = kmeans.fit_predict(kmeans_df)
+    print("Centers",kmeans.cluster_centers_)
+    distortion.append(kmeans.inertia_)
+    print('Silhouette score for {} cluster ='.format(i),silhouette_score(kmeans_df,y_km))
+    kmeans_df_array = np.array(kmeans_df)
+    for j in range(len(kmeans.cluster_centers_)):
+        plt.scatter(kmeans_df_array[y_km==j,0],kmeans_df_array[y_km==j,1],marker='o',c=c[j])
+    plt.title('KMeans Clustering with {} clusters'.format(i))
+    plt.show()
+print('Distortion-->',distortion)
+
+a=np.arange(2,unique_grades)
+plt.plot(a,distortion)
+plt.grid()
 plt.show()
-#plt.scatter(train[:,0],train[:,1])
 
-'''correct = 0
-for i in range(len(test)):
-    predict_me = np.array(test[i].astype(float))
-    predict_me = predict_me.reshape(-1, len(predict_me))
-    prediction = kmeans.predict(predict_me)
-    if prediction[0] == test[i]:
-        correct += 1
 
-print(correct/len(test))'''
+#Agglomerative Clustering technique
+ac_df = df.copy()
+ac_df = df.drop(['student_id','Grade'],axis=1)
+for i in range(2,4):
+    ac = AgglomerativeClustering(n_clusters=i,affinity='euclidean', linkage='complete')
+    labels = ac.fit_predict(ac_df) 
+    for j in range(i):
+        plt.scatter(ac_df.iloc[labels == j,0], ac_df.iloc[labels == j,1])
+        plt.title('Agglomerative Clustering with {} clusters'.format(i))
+        plt.plot()
+    plt.show()
+
